43e50229bc3d820739d8aadf2e001b63aadeb0b0
Raphael Arcanjo <raphael_arcanjo@live.com>
2021-02-08 19:54:43 -0300

    Usando arquivos static

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('create', TaskView.create, name='create'),
     path('update/<int:id>', TaskView.update, name='update'),
     path('delete/<int:id>', TaskView.delete, name='delete'),
-    path('css/<str:file>.css', TaskView.stylesheet, name='css'),
-    path('js/<str:file>.js', TaskView.scripts, name='js'),
 ]
