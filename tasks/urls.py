f406be54fd66c5881a4c2006686a67b9a0c3d3da
raphaelarcanjo <raphael_arcanjo@live.com>
2021-03-04 19:59:50 -0300

    Sistema de login para acessar as tarefas

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import TaskView
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', TaskView.list, name='list'),
-    path('create', TaskView.create, name='create'),
-    path('update/<int:id>', TaskView.update, name='update'),
-    path('delete/<int:id>', TaskView.delete, name='delete'),
+    path('', views.list, name='list'),
+    path('create', views.create, name='create'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page="login"), name="logout"),
+    path('register/', views.register, name="register"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from .models import Task
 from .forms import TaskForm
 from datetime import datetime
@@ -6,48 +8,61 @@ from datetime import datetime
 # Create your views here.
 
 
-class TaskView():
-    def list(request):
-        tasks = Task.objects.all()
-        dt = datetime.now()
-        data = {
-            'tasks': tasks,
-            'date': dt.date(),
-            'time': dt.time(),
-        }
+@login_required
+def list(request):
+    tasks = Task.objects.all()
+    dt = datetime.now()
+    data = {
+        'tasks': tasks,
+        'date': dt.date(),
+        'time': dt.time(),
+    }
 
-        return render(request, 'list.html', data)
+    return render(request, 'list.html', data)
 
-    def create(request):
-        form = TaskForm(request.POST or None)
+
+@login_required
+def create(request):
+    form = TaskForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list')
+
+    return render(request, 'create.html', {'form': form})
+
+
+@login_required
+def update(request, id):
+    task = Task.objects.get(id=id)
+    form = TaskForm(request.POST or None, instance=task)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list')
+
+    return render(request, 'update.html', {'form': form, 'task': task})
+
+
+@login_required
+def delete(request, id):
+    task = Task.objects.get(id=id)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('list')
+
+    return render(request, 'confirm_delete.html', {'task': task})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
 
         if form.is_valid():
             form.save()
-            return redirect('list')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
 
-        return render(request, 'create.html', {'form': form})
-
-    def update(request, id):
-        task = Task.objects.get(id=id)
-        form = TaskForm(request.POST or None, instance=task)
-
-        if form.is_valid():
-            form.save()
-            return redirect('list')
-
-        return render(request, 'update.html', {'form': form, 'task': task})
-
-    def delete(request, id):
-        task = Task.objects.get(id=id)
-
-        if request.method == 'POST':
-            task.delete()
-            return redirect('list')
-
-        return render(request, 'confirm_delete.html', {'task': task})
-
-    def stylesheet(request, file):
-        return render(request, 'assets/css/' + file + '.css', {}, content_type='text/css')
-
-    def scripts(request, file):
-        return render(request, 'assets/js/' + file + '.js', {}, content_type='application/javascript')
+    return render(request, 'registration/register.html', {'form': form})
